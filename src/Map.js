69c4e1eff81e31d8658f4e904e12018922117c2b
BanditCook <tanguy.lalexandre@gmail.com>
2023-06-14 22:34:44 +0100

    Add a start of logic for the isometric system of the map

--- src/Map.js
+++ src/Map.js
@@ -5,24 +5,24 @@ export default class MapScene extends Phaser.Scene {
   constructor() {
     super("mapScene");
   }
+
   preload() {
     this.load.tilemapTiledJSON("map", "assets/json/Map.json");
     this.load.image("tiles", "assets/images/BaseSpritev2.png");
     this.load.image("tower", "assets/images/Tower.png");
   }
-// @ts-ignore
-    
+
   create() {
-    var map = this.make.tilemap({ key: "map" });
+    const map = this.make.tilemap({ key: "map" });
     if (!map) {
       console.error("Failed to load tilemap");
       return;
     }
-    var tileset = map.addTilesetImage("BaseTilesV2", "tiles");
+    const tileset = map.addTilesetImage("BaseTilesV2", "tiles");
 
-    var layerNames = [];
-    var i = 0;
-    var layerName = i + "layer";
+    const layerNames = [];
+    let i = 0;
+    let layerName = i + "layer";
 
     while (map.getLayer(layerName)) {
       layerNames.push(layerName);
@@ -30,21 +30,44 @@ export default class MapScene extends Phaser.Scene {
       layerName = i + "layer";
     }
 
-    layerNames.forEach((layerName) => {
-      var layer = map.createLayer(layerName, tileset);
+    const tileLayer = layerNames.map((layerName) => {
+      return map.createLayer(layerName, tileset);
     });
 
-    var mapWidth = map.widthInPixels;
-    var mapHeight = map.heightInPixels;
-    var centerX = this.cameras.main.width / 2;
-    var centerY = this.cameras.main.height / 2;
-    var offsetX = centerX - mapWidth / 1.4;
-    var offsetY = centerY - mapHeight / 1.2;
-    this.cameras.main.scrollX = offsetX;
-    this.cameras.main.scrollY = offsetY;
+    tileLayer.forEach((layer) => {
+      layer.forEachTile((tile) => {
+        const tileX = Math.floor(tile.pixelX + tile.width / 2) / 32;
+        const tileY = Math.floor(tile.pixelY + tile.height / 2) / 32;
+        tile.properties.coordinates = { x: tileX, y: tileY };
+      });
+    });
 
+    const offsetX = 50; // Set the desired offset in the X-axis
+    const offsetY = 50; // Set the desired offset in the Y-axis
 
-    const newTower = new Tower(this, 200, 500);
+    const mapWidth = map.widthInPixels;
+    const mapHeight = map.heightInPixels;
+
+    // this.cameras.main.setBounds(
+    //   -offsetX,
+    //   -offsetY,
+    //   mapWidth + offsetX * 2,
+    //   mapHeight + offsetY * 2
+    // );
+    this.cameras.main.centerOn(mapWidth / 8.5, mapHeight / 1.5);
+
+    tileLayer.forEach((layer) => {
+      layer.setInteractive();
+      layer.on("pointerdown", (pointer, x, y, event) => {
+        const tile = layer.getTileAtWorldXY(x, y);
+        if (tile) {
+          const { x: tileX, y: tileY } = tile.properties.coordinates;
+          console.log(`Tile Coordinates: x=${tileX}, y=${tileY}`);
+        }
+      });
+    });
+
+    const newTower = new Tower(this, 300, 500);
     this.add.existing(newTower);
   }
 }
