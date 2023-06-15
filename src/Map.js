87a8313b24712b8c3eecacf620b4645b3f920260
Darrach Barneveld <darrachbarneveld91@gmail.com>
2023-06-15 16:38:36 +0100

    added line path

--- src/Map.js
+++ src/Map.js
@@ -16,21 +16,23 @@ export default class MapScene extends Phaser.Scene {
     this.load.image("tiles", "assets/images/2Dsprites.png");
 
     this.load.image("turret", "assets/images/Turret2D.png");
-
-
-
   }
 
   create() {
     const map = this.make.tilemap({ key: "map" });
     const tileset = map.addTilesetImage("2Dsprites", "tiles", 32, 32);
 
-
     const layer1 = map.createLayer(0, tileset);
     const layer2 = map.createLayer(1, tileset);
 
     layer1.setInteractive(); // Make layer1 clickable
     layer1.on("pointerdown", this.onTileClicked, this); // Add click event listener
+
+    let graphics = this.add.graphics();
+    path = this.add.path(145, MAP_HEIGHT);
+    drawWaypointPath();
+    graphics.lineStyle(5, 0xffff00, 1);
+    path.draw(graphics);
   }
 
   onTileClicked(pointer) {
@@ -46,7 +48,6 @@ export default class MapScene extends Phaser.Scene {
 
     // Place the turret at the center of the clicked tile
     const turret = new Turret(this, centerX, centerY, "turret");
-
   }
 }
 
