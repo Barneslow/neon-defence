ca419ba8d059e9d70b93a83ec4dfbab625bf2572
devancadman@outlook.com <devancadman@outlook.com>
2023-06-23 00:33:47 +0100

    Updated Modal & Music JS

--- src/intro.js
+++ src/intro.js
@@ -1,9 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
   const startButton = document.getElementById("start-button");
-  const howToPlayButton = document.getElementById("how-to-play-button");
-  const soundButton = document.getElementById("sound-button");
-  const musicButton = document.getElementById("music-button");
-  const creditsButton = document.getElementById("credits-button");
   const menu = document.getElementById("menu");
 
   // Add event listener to the start button
@@ -13,51 +9,6 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("introHeader").style.display = "none";
   });
 
-  // Add event listener to the how to play button
-  howToPlayButton.addEventListener("click", function (event) {
-    event.preventDefault();
-    // Show the how to play popup
-    // Implement the desired behavior, such as showing a modal or displaying content on the page
-    console.log("How to Play button clicked");
-  });
-
-  // Add event listener to the sound button
-  soundButton.addEventListener("click", function (event) {
-    event.preventDefault();
-    const soundButtonText = soundButton.textContent.trim();
-    if (soundButtonText === "Sound: ON") {
-      soundButton.textContent = "Sound: OFF";
-      // Turn off the sound
-      console.log("Sound turned off");
-    } else {
-      soundButton.textContent = "Sound: ON";
-      // Turn on the sound
-      console.log("Sound turned on");
-    }
-  });
-
-  // Add event listener to the music button
-  musicButton.addEventListener("click", function (event) {
-    event.preventDefault();
-    const musicButtonText = musicButton.textContent.trim();
-    if (musicButtonText === "Music: ON") {
-      musicButton.textContent = "Music: OFF";
-      // Turn off the music
-      console.log("Music turned off");
-    } else {
-      musicButton.textContent = "Music: ON";
-      // Turn on the music
-      console.log("Music turned on");
-    }
-  });
-
-  // Add event listener to the credits button
-  creditsButton.addEventListener("click", function (event) {
-    event.preventDefault();
-    // Show the credits popup
-    // Implement the desired behavior, such as showing a modal or displaying content on the page
-    console.log("Credits button clicked");
-  });
 });
 
 // Stars Background
@@ -90,32 +41,26 @@ const render = () => {
 requestAnimationFrame(render);
 window.addEventListener("resize", () => requestAnimationFrame(render)); 
 
-// Music / Volume Button
-const volume = document.getElementById("volume");
+// Music / Music Button
+const music = document.getElementById("music");
 const audio = document.getElementById("synthwave-track");
-const musicButton = document.getElementById("music-button");
 
-const volumeToggle = () => {
-  if (volume.classList.contains("playing")) {
-    volume.classList.remove("playing");
+const musicToggle = () => {
+  if (music.classList.contains("playing")) {
+    music.classList.remove("playing");
     audio.pause();
-    musicButton.textContent = "Music: OFF";
   } else {
-    volume.classList.add("playing");
+    music.classList.add("playing");
     audio.play();
-    musicButton.textContent = "Music: ON";
   }
 };
 
-volume.addEventListener("pointerdown", () => volumeToggle());
-volume.addEventListener("keydown", (e) => {
+music.addEventListener("pointerdown", () => musicToggle());
+music.addEventListener("keydown", (e) => {
   if (e.code === "Space" || e.code === "Enter") {
-    volumeToggle();
+    musicToggle();
   }
 });
-
-musicButton.addEventListener("click", () => volumeToggle());
-
 
 // Dropdown
 const dropdownButton = document.querySelector(".dropbtn");
@@ -134,30 +79,34 @@ window.addEventListener("click", (event) => {
   }
 });
 
-
-// Function to open a modal
-const openModal = (modalElement) => {
-  modalElement.setAttribute("open", "");
-};
-
-// Function to close a modal
-const closeModal = (modalElement) => {
-  modalElement.removeAttribute("open");
-};
-
-// Attach event listeners to the buttons
 const attachModalEvents = (buttonElement, modalElement) => {
   const closeButton = modalElement.querySelector(".close-button");
 
-  buttonElement.addEventListener("click", () => openModal(modalElement));
-  closeButton.addEventListener("click", () => closeModal(modalElement));
+  buttonElement.addEventListener("click", () => {
+    modalElement.setAttribute("open", "");
+  });
+
+  closeButton.addEventListener("click", () => {
+    modalElement.removeAttribute("open");
+  });
 };
 
-// Usage example:
+// Leaderboard:
+const leaderboardButton = document.getElementById("leaderboard-button");
+const modalLeaderboard = document.getElementById("modalLeaderboard");
+attachModalEvents(leaderboardButton, modalLeaderboard);
+
+// How to play:
 const howToPlayButton = document.getElementById("how-to-play-button");
 const modalHowToPlay = document.getElementById("modalHowToPlay");
 attachModalEvents(howToPlayButton, modalHowToPlay);
 
+// Credits:
 const creditsButton = document.getElementById("credits-button");
 const modalCredits = document.getElementById("modalCredits");
 attachModalEvents(creditsButton, modalCredits);
+
+// Sign in:
+const signInButton = document.getElementById("signin-btn");
+const signInModal = document.getElementById("modalSignin");
+attachModalEvents(signInButton, signInModal);
