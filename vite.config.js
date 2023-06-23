7586f79be06e29d2ad6b70399ff61e2e70c2fc1c
Darrach Barneveld <darrachbarneveld91@gmail.com>
2023-06-23 12:55:32 +0100

    testing nesting index

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        game: resolve(__dirname, "game.html"),
+        nested: resolve(__dirname, "nested/index.html"),
       },
     },
   },
