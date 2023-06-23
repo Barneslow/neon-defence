ea91239145a09558cc27da463683765a89b85778
Darrach Barneveld <darrachbarneveld91@gmail.com>
2023-06-23 12:50:03 +0100

    required path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
+const { resolve } = require("path");
 
 export default defineConfig({
   plugins: [],
   server: { host: "https://neon-defence.netlify.app/", port: 8000 },
   clearScreen: false,
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        game: resolve(__dirname, "game.html"),
+      },
+    },
+  },
 });
 // https://neon-defence.netlify.app/
