e296d0d41b0b68da4c4e3cc8c71b8dfd28d68548
Darrach Barneveld <darrachbarneveld91@gmail.com>
2023-06-24 18:29:30 +0100

    removed lint

--- src/parcelSpriteImports.js
+++ src/parcelSpriteImports.js
@@ -1,68 +1,40 @@
-// @ts-ignore
 import gameMap from "../assets/json/2DTowerDefense.json";
-// @ts-ignore
+
 import map2Dsprites from "../assets/images/2Dsprites.png";
 
 // Enemy imports
-// @ts-ignore
 import boss from "../assets/images/enemies/boss.png";
-// @ts-ignore
 import drone from "../assets/images/enemies/drone.png";
-// @ts-ignore
 import golem from "../assets/images/enemies/golem.png";
-// @ts-ignore
 import heavyBot from "../assets/images/enemies/heavybot.png";
-// @ts-ignore
 import robot from "../assets/images/enemies/robot.png";
-// @ts-ignore
 import spider from "../assets/images/enemies/spider.png";
 
 // Power imports
-// @ts-ignore
 import electricTowerActive from "../assets/images/turrets/ElectricTowerActive.png";
-// @ts-ignore
 import electricTowerInactive from "../assets/images/turrets/ElectricTowerInactive.png";
-// @ts-ignore
 import fireTowerActive from "../assets/images/turrets/FireTowerActive.png";
-// @ts-ignore
 import fireTowerInActive from "../assets/images/turrets/FireTowerInactive.png";
-// @ts-ignore
 import freezeTowerActive from "../assets/images/turrets/FreezeTowerActive.png";
-// @ts-ignore
 import freezeTowerInactive from "../assets/images/turrets/FreezeTowerInactive.png";
 
 // Tower imports
-// @ts-ignore
 import turret from "../assets/images/turrets/Turret2D.png";
-// @ts-ignore
 import turret2 from "../assets/images/turrets/Turret2Dlvl2.png";
-// @ts-ignore
 import turret3 from "../assets/images/turrets/Turret2Dlvl3.png";
-// @ts-ignore
 import laser from "../assets/images/turrets/LaserTurret.png";
-// @ts-ignore
 import laser2 from "../assets/images/turrets/LaserTurretlvl2.png";
-// @ts-ignore
 import laser3 from "../assets/images/turrets/LaserTurretlvl3.png";
-// @ts-ignore
 import shotgun from "../assets/images/turrets/ShotGunTurret.png";
-// @ts-ignore
 import shotgun2 from "../assets/images/turrets/ShotGunTurretlvl2.png";
-// @ts-ignore
 import shotgun3 from "../assets/images/turrets/ShotGunTurretlvl3.png";
-// @ts-ignore
 import human from "../assets/images/turrets/HumanTurret.png";
-// @ts-ignore
 import human2 from "../assets/images/turrets/HumanTurretlvl2.png";
-// @ts-ignore
 import human3 from "../assets/images/turrets/HumanTurretlvl3.png";
 
 // Bullet Imports
-// @ts-ignore
 import bullet from "../assets/images/Bullet.png";
-// @ts-ignore
 import shotgunBullet from "../assets/images/ShotGunBullet.png";
-// @ts-ignore
 import humanBullet from "../assets/images/HumanBullet.png";
 
 export {
