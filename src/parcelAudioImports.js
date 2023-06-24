d7e069cb8e709f30a6343eaab40878b8b3f004a1
Darrach Barneveld <darrachbarneveld91@gmail.com>
2023-06-24 18:54:48 +0100

    testing

--- src/parcelAudioImports.js
+++ src/parcelAudioImports.js
@@ -1,4 +1,4 @@
-import electricity from "/assets/sounds/electricity.mp3";
+import electricity from "/assets/sounds/Electricity.mp3";
 
 import fire from "/assets/sounds/fire.mp3";
 
@@ -8,7 +8,7 @@ import powerUp from "/assets/sounds/power-up.mp3";
 
 import laser from "/assets/sounds/laser.mp3";
 
-import bullet from "/assets/sounds/bulletsound.mp3";
+import bullet from "/assets/sounds/BulletSound.mp3";
 
 import dead from "/assets/sounds/dead-enemy.mp3";
 
