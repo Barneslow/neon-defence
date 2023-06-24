02af8d21512ed289d6340796bf446a650013a5cd
Darrach Barneveld <darrachbarneveld91@gmail.com>
2023-06-24 18:05:14 +0100

    testing path audio

--- src/parcelAudioImports.js
+++ src/parcelAudioImports.js
@@ -1,26 +1,26 @@
 // @ts-ignore
-import electricity from "../assets/sounds/electricity.mp3";
+import electricity from "/assets/sounds/electricity.mp3";
 
 // @ts-ignore
 
-import fire from "../assets/sounds/fire.mp3";
+import fire from "/assets/sounds/fire.mp3";
 // @ts-ignore
 
-import freeze from "../assets/sounds/freeze.mp3";
+import freeze from "/assets/sounds/freeze.mp3";
 // @ts-ignore
 
-import powerUp from "../assets/sounds/power-up.mp3";
+import powerUp from "/assets/sounds/power-up.mp3";
 // @ts-ignore
 
-import laser from "../assets/sounds/laser.mp3";
+import laser from "/assets/sounds/laser.mp3";
 // @ts-ignore
 
-import bullet from "../assets/sounds/bulletsound.mp3";
+import bullet from "/assets/sounds/bulletsound.mp3";
 
 // @ts-ignore
-import dead from "../assets/sounds/dead-enemy.mp3";
+import dead from "/assets/sounds/dead-enemy.mp3";
 
 // @ts-ignore
-import deadboss from "../assets/sounds/dead-boss.mp3";
+import deadboss from "/assets/sounds/dead-boss.mp3";
 
 export { electricity, fire, freeze, powerUp, laser, bullet, dead, deadboss };
